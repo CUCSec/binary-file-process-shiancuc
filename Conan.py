f98dbf447c7a963c28e2a708586b55005b18b95a
shian <1019988975@qq.com>
2019-05-14 22:36:20 +0800

    修改完成

--- Conan.py
+++ Conan.py
@@ -2,11 +2,37 @@ import struct
 
 
 def tamper(student_id):
-  pass
+  with open(r'E:\Python\binary-file-process-shiancuc\lenna.bmp','r+b') as f:
+    f.seek(54,0)
+    #f.write(b'\x00\x00\x00')
+    f.seek(6,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(27,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(0,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(21,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(0,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(0,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(0,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(3,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(6,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(27,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(27,1)
+    f.write(b'\x00\x00\x00')
+    f.seek(18,1)
+    f.write(b'\x00\x00\x00')
 
 
 def detect():
-  with open('lenna.bmp', 'rb') as f:
+  with open(r'E:\Python\binary-file-process-shiancuc\lenna.bmp', 'rb') as f:
     bmp_file_header = f.read(14)
 
     bm, size, r1, r2, offset = struct.unpack('<2sIHHI', bmp_file_header)
